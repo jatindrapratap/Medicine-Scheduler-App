849d441b72748bccb736ed671f319fbe197eb4d6
Jatindra Pratap Singh <jatindrapratap99@gmail.com>
2024-09-15 12:43:27 +0530

    Adjust styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 
 import React from "react";
 import MedicineScheduler from "./MedicineScheduler";
-
+import "./App.css"
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <MedicineScheduler />
     </div>
   );
